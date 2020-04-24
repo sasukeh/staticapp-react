44cb7b682aa868ccce11479e557a26fdb43bace5
Burke Holland <burkeholland@gmail.com>
2020-04-24 14:24:39 -0500

    adding the api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import Message from "./Message";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <Message></Message>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
